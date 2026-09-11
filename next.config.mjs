a32058848d62c43d2eb0f1e4f2dac69bb84b378c
Viraj030 <virajasolkar0181@gmail.com>
2026-09-11 13:40:01 +0530

    Configure static export out build for mPanel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
   images: {
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
 };
 
